2005d7e773629f7830b703b2a6d3410ec5e008d2
Manjari <manjari@Manjari.localdomain>
2026-06-04 16:48:23 +0300

    Feature: Created the components and styles folder and created an outline for the forms

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddMovies from "./components/addMovies-form";
+
+
+function App(){
+
+  return(
+    <div>
+      <AddMovies/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/addMovies-form.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import '../styles/form.css'
+
+function AddMovies(){
+
+    return(
+        <div>
+            <form className="form">
+                <h2>Add a Movie</h2>
+              <div className="name-genre">
+                    <label htmlFor="movie">Movie : <input type="text" id="movie" placeholder="Enter Movie Title" /></label>
+                    <label htmlFor="genre">Add a Movie:
+
+                        <select name="genre" id="genre">
+                            <option value="Action">Action</option>
+                            <option value="Drama">Drama</option>
+                            <option value="Animation">Animation</option>
+                            <option value="Crime">Crime</option>
+                            <option value="Fantasy">Fantasy</option>
+                        </select>
+                    </label>
+              </div>
+                
+                <div className="url-status">
+                    <label htmlFor="imageUrl">Image Url <input type="url" name="imageUrl" id="imageUrl" placeholder="Enter Image url" /></label>
+                    <label htmlFor="status"> Choose Status :
+                        <select name="status" id="status">
+                            <option value="Completed">Completed</option>
+                            <option value="Watched">Watched</option>
+                            <option value="Ongoing">OnGoing</option>
+                        </select>
+                    </label>
+
+                </div>
+
+                <label htmlFor="description" id="desc"> Description
+                    <textarea name="description" id="description"placeholder="Add Movie description"></textarea>
+                </label>
+                <div>
+                    <button type="submit">Add Movie</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default AddMovies;
