77007b7e8de00fec05ca61239d213776c845b8a6
Manjari <manjari@Manjari.localdomain>
2026-06-07 14:08:48 +0300

    added the fetch request to post data to the api and database

--- src/App.jsx
+++ src/App.jsx
@@ -27,17 +27,23 @@ function App(){
 
     // code to handle submit and post changes to the api
       function handleSubmit(event){
+        event.preventDefault();
         // condition to check all inputs are filled
         (movie.title !== "" && movie.releaseYear !== "" &&
           movie.description !== "" && movie.rating !== ""
-        )?
+        )? (
+          addMovie(movie)
+            .then((data) => {
+              setUpdade([...update, data])
+            })
         // code to update the parameters for forms
-        setMovie({
-          "title": "", "genre": "Action",
-          "releaseYear": "", "posterUrl": "",
-          "status": "Completed", "rating": "",
-          "description": "",
-        }) : alert("Fill in all the required fields ")
+              (setMovie({
+                "title": "", "genre": "Action",
+                "releaseYear": "", "posterUrl": "",
+                "status": "Completed", "rating": "",
+                "description": "",
+              }))
+        ): alert("Fill in all the required fields ")
       }
   return(
     <div>
