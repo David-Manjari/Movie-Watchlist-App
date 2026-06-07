34bfbd7bdd94302501bf245a967c088730944eac
Manjari <manjari@Manjari.localdomain>
2026-06-07 13:02:06 +0300

    created a function that handles input from form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AddMovies from "./components/addMovies-form";
 
 
 function App(){
 
+  // code to get content from the form
+  const [movie, setMovie] = useState({
+    "title": "","genre": "",
+    "releaseYear": "", "posterUrl":"",
+    "status": "","rating": "",
+    "description":"",
+})
+
+    function handleSubmit(event){
+      setMovie({...movie, [event.target.name]:event.target.value})
+    }
+    
   return(
     <div>
       <AddMovies/>
